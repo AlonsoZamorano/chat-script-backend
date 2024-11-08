7ba5abe7e6f6fde16d4c0c8427933cfae98b3046
Alonso Zamorano <alonso.zamorano@uc.cl>
2024-11-08 03:46:13 -0300

    cambio a csv

--- startComand.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Update the package list and install system dependencies
-sudo apt-get update
-sudo apt-get install -y swig libfaiss-dev build-essential
-
-# Install Python dependencies from requirements.txt
-pip install -r requirements.txt
