3170b040979878324fdea393f7dd801824a2f07b
Alonso Zamorano <alonso.zamorano@uc.cl>
2024-11-08 03:45:09 -0300

    cambio a csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import faiss
+import csv
+from scipy.spatial.distance import cdist
 import numpy as np
 import requests
 from pydantic import BaseModel
@@ -22,8 +23,28 @@ app.add_middleware(
 )
 
 d = 768
-index = faiss.IndexFlatL2(d)
+def cargar_vectores_csv(csv_file_path):
+    vectores = []
+    with open(csv_file_path, mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            vectores.append(np.array([float(x) for x in row]))
+    return np.array(vectores)
 
+def encontrar_vectores_cercanos(vector_consulta, vectores, k=5):
+    # Calcular la distancia euclidiana entre el vector de consulta y los demás vectores
+    distancias = cdist([vector_consulta], vectores, metric='euclidean').flatten()
+
+    # Obtener los índices de los k vectores más cercanos
+    indices_cercanos = np.argsort(distancias)[:k]
+
+    # Obtener los k vectores más cercanos y sus distancias
+    vectores_cercanos = vectores[indices_cercanos]
+    distancias_cercanas = distancias[indices_cercanos]
+    
+    return vectores_cercanos, indices_cercanos
+
+vectores = cargar_vectores_csv("vectores.csv")
 fragmentos_texto = np.load("fragmentos_texto.npy", allow_pickle=True)
 
 @app.get("/")
@@ -44,11 +65,14 @@ async def query(query: Query):
         raise HTTPException(status_code=500, detail="Error al generar embedding")
     print("EMBEDDING")
     embedding = responseEmbedding.json()["embeddings"]
-    D, I = index.search(np.array(embedding, dtype='float32'), 5)
+    
+    # Encontrar los 5 vectores más cercanos al vector de la consulta
+    vectores_cercanos, indices_cercanos = encontrar_vectores_cercanos(embedding[0], vectores, k=5)
+    print(vectores_cercanos)
 
     retrieved_fragments = []
     # Aplanar I para que sea una lista unidimensional de índices
-    for indice in I[0]:
+    for indice in indices_cercanos:
         retrieved_fragments.append(fragmentos_texto[indice])
 
     prompt = "Eres un asistente experto en peliculas. Por favor, responde a la siguiente pregunta: " + texto + ". Además te entregamos el siguiente contexto" + "\n".join(retrieved_fragments)
