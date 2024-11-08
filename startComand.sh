de70b31e2f5cb16199e2f056a95a4b72bb77da90
Alonso Zamorano <alonso.zamorano@uc.cl>
2024-11-08 02:50:39 -0300

    docker

--- startComand.sh
+++ startComand.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Update the package list and install system dependencies
-apt-get update
-apt-get install -y swig libfaiss-dev build-essential
+sudo apt-get update
+sudo apt-get install -y swig libfaiss-dev build-essential
 
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
