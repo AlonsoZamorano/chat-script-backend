f7e2deb9766cce9bd4840dac736734b8522986c4
Alonso Zamorano <alonso.zamorano@uc.cl>
2024-11-08 01:16:07 -0300

    fix

--- main.py
+++ main.py
@@ -26,15 +26,7 @@ d = 768
 index = faiss.IndexFlatL2(d)
 model = SentenceTransformer("nomic-ai/nomic-embed-text-v1.5", trust_remote_code=True)
 
-all_fragments = []
-# Cargamos los guiones
-for file in os.listdir("cleaned_scripts"):
-    with open(f"./cleaned_scripts/{file}", 'r', encoding='utf-8') as f:
-        texto = f.read()
-
-    # Dividimos el texto en fragmentos de 500 palabras
-    fragmentos = [texto[i:i+500] for i in range(0, len(texto), 500)]
-    all_fragments.extend(fragmentos)
+fragmentos_texto = np.load("fragmentos_texto.npy", allow_pickle=True)
 
 @app.get("/")
 async def root():
@@ -48,15 +40,18 @@ async def query(query: Query):
     embedding = model.encode("search_document: " + texto)
     D, I = index.search(np.array([embedding], dtype='float32'), 5)
 
-    retrieved_fragmets = [all_fragments[i] for i in I]
+    retrieved_fragments = []
+    # Aplanar I para que sea una lista unidimensional de índices
+    for indice in I[0]:
+        retrieved_fragments.append(fragmentos_texto[indice])
 
-    prompt = "Eres un asistente experto en peliculas. Por favor, responde a la siguiente pregunta: " + texto + ". Además te entregamos el siguiente contexto" + " ".join(retrieved_fragmets)
+    prompt = "Eres un asistente experto en peliculas. Por favor, responde a la siguiente pregunta: " + texto + ". Además te entregamos el siguiente contexto" + "\n".join(retrieved_fragments)
      
     # Pedimos a la API que nos genere una respuesta con el contexto generado
     # tormenta.ing.puc.cl/api/generate POST
     print(prompt)
     
-    response = requests.post("http://tormenta.ing.puc.cl/api/generate", data={
+    response = requests.post("http://tormenta.ing.puc.cl/api/generate", json={
         "model": "llama3.2",
         "prompt": prompt,
         "stream": "false",
